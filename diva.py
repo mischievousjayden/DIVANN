390638ae75d990eb7d2a097ab6895fbdc3c192a8
Jayden <mischievousjayden@gmail.com>
2017-01-31 08:53:45 -0500

    added response rule

--- diva.py
+++ diva.py
@@ -3,16 +3,16 @@ import tensorflow as tf
 from data import Data 
 
 # parameters
-training_epochs = 20
-total_batch = 13
-display_step = 10
+num_set_inputs = 13
+training_epochs = 100
+display_step = 5
 
 # network parameters
 num_features = 3
 num_hidden = 8
 
-n_input = 3
-n_hidden = 8
+# response rule beta
+beta = 0.8
 
 # tf graph input
 X = tf.placeholder("float", [None, num_features], name="x-input")
@@ -40,8 +40,18 @@ def decodeToMultiDim(x):
     output2 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights["decoder2"]), biases["decoder2"]))
     return [output1, output2]
 
-def getPrediction(outputs, current_class):
-    return outputs[current_class]
+def resposeRule(outputs, label, beta):
+    o1 = tf.pow(outputs[0] - label, [2, 2, 2])
+    o2 = tf.pow(outputs[1] - label, [2, 2, 2])
+    ssqerror = [o1, o2]
+    diversities = tf.exp(beta*(tf.abs(outputs[0] - outputs[1])))
+    fweights = tf.nn.softmax(diversities)
+    accuracy1 = 1.0 / tf.reduce_sum(tf.mul(o1, fweights))
+    accuracy2 = 1.0 / tf.reduce_sum(tf.mul(o2, fweights))
+    return tf.nn.softmax([accuracy1, accuracy2])
+
+# def getPrediction(outputs, current_class):
+#     return outputs[current_class]
 
 # construct model
 with tf.name_scope("encoder_layer") as scope:
@@ -56,6 +66,12 @@ y_pred = tf.cond(current_class > 0 , lambda: decoder_op[1], lambda: decoder_op[0
 
 # target
 y_true = X
+
+# accuracy
+with tf.name_scope("accuracy") as scope:
+    accuracy_vec = resposeRule(decoder_op, X, beta)
+    accuracy = tf.cond(current_class > 0 , lambda: accuracy_vec[1], lambda: accuracy_vec[0])
+    accuracy_summ = tf.scalar_summary("accuracy", accuracy)
 
 # define cost
 with tf.name_scope("cost") as scope:
@@ -81,20 +97,19 @@ y_hist = tf.histogram_summary("prediction", y_pred)
 # initializing the variables
 init = tf.initialize_all_variables()
 
-
 with tf.Session() as sess:
     merged = tf.merge_all_summaries()
-    writer = tf.train.SummaryWriter("./logs/diva", sess.graph_def)
 
-    sess.run(init)
-
-    for i in range(total_batch):
+    for i in range(num_set_inputs):
+        logfilename = "./logs/set{}".format(i)
+        writer = tf.train.SummaryWriter(logfilename, sess.graph_def)
+        sess.run(init)
         for epoch in range(training_epochs):
             for j, current_input in enumerate(Data.Stimuli[i]):
-                _, c = sess.run([optimizer, cost], feed_dict={X:[current_input], current_class:Data.Assignments[j]})
+                _, c, a = sess.run([optimizer, cost, accuracy], feed_dict={X:[current_input], current_class:Data.Assignments[j]})
                 if epoch % display_step == 0:
                     summary = sess.run(merged, feed_dict={X:[current_input], current_class:Data.Assignments[j]})
                     writer.add_summary(summary, epoch)
-                    print("input:", i, "Epoch:", ' %04d' % (epoch+1), "cost=", "{:.9f}".format(c), "label=", Data.Assignments[j])
+                    print("input_set: {}".format(i), "Epoch: {}".format(epoch+1), "cost: {:.9f}".format(c), "accuracy={:.9f}".format(a), "label={}".format(Data.Assignments[j]))
     print("Optimization Finished!")
 
--- makefile
+++ makefile
@@ -3,7 +3,7 @@ run: diva.py
 	python3 diva.py
 
 run_tensorboard:
-	tensorboard --logdir=logs/diva
+	tensorboard --logdir=logs/set1
 
 clean:
 	rm -rf *.pyc __pycache__ logs
